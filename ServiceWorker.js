8748f1bfad8b51f37206a4128862da034b5f33d0
L U F i A S <saifulanik@icloud.com>
2025-03-25 02:10:52 +0600

    cache data auto clean

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,40 @@
+const cacheName = "Spear-Spear On Near-2";
+const contentToCache = [
+    "Build/SpearOnNearBattleRoyal.loader.js",
+    "Build/SpearOnNearBattleRoyal.framework.js",
+    "Build/SpearOnNearBattleRoyal.data",
+    "Build/SpearOnNearBattleRoyal.wasm",
+    "TemplateData/style.css"
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+  e.respondWith((async function () {
+    if (e.request.url.includes('Build/') || e.request.url.includes('TemplateData/')) {
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      const response = await caches.match(e.request);
+      if (response) { return response; }
+      else {
+        let _res = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, _res.clone());
+        return _res;
+      }
+    }
+    else {
+      let res = await fetch(e.request);
+      console.log(`[Service Worker] Downloading new resource: ${e.request.url}`);
+      return res;
+    }
+  })());
+});
